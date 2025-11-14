1ab280cb129890ca1f0b6a3568e8c92e6345acda
nawhizz <nawhizz@gmail.com>
2025-11-15 00:17:38 +0900

    fix: PostCSS 플러그인 from 옵션 경고 해결
    - postcss.config.js를 함수 형태로 변경
    - PostCSS가 from 옵션을 자동으로 전달하도록 수정
    - Vercel 빌드 시 경고 메시지 제거

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+// PostCSS 설정을 함수 형태로 변경하여 from 옵션 자동 전달
+export default (ctx) => {
+  return {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  };
+};
